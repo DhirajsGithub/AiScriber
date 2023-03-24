26756c9e8bafd828d11c495773fa918e9eb2a5ba
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2023-03-24 13:18:41 +0530

    async audio conversion done

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
 const path = require("path");
-const { exec } = require("child_process");
-const multer = require("multer");
 const upload = require("./server/multer/multerUpload.js");
-const convertToAudio = require("./server/ffmpegCommands/videoToAudio.js");
-const isConverted = require("./server/ffmpegCommands/videoToAudio.js");
 const app = express();
 // const upload = multer({ dest: "./client/uploads" });
 app.get("/", (req, res) => {
@@ -12,6 +10,15 @@ app.get("/", (req, res) => {
     root: path.join(__dirname),
   };
   const fileName = "./server/sendVideo.html";
+  fs.unlinkSync(__dirname + "/client/uploads/audio.wav", (err) => {
+    if (err) {
+      res.sendFile(fileName, options, function (err2) {
+        if (err2) {
+          console.log(err2);
+        }
+      });
+    }
+  });
   res.sendFile(fileName, options, function (err) {
     if (err) {
       console.log(err);
@@ -23,17 +30,29 @@ app.get("/foundAudio", (req, res) => {
   const options = {
     root: path.join(__dirname),
   };
-  const fileName = "./client/uploads/audio.wav";
+  const fileName = "/client/uploads/audio.wav";
   res.sendFile(fileName, options, function (err) {
     if (err) {
-      console.log("Path not yet created");
+      console.log("Underway");
     }
   });
 });
 
 app.post("/send_video", upload.single("lecture"), (req, res) => {
-  convertToAudio("Lecture.mp4");
-  res.redirect("/foundAudio");
+  let newFileName = __dirname + "/client/uploads/Lecture.mp4";
+  let targetLocn = __dirname + "/client/uploads/audio.wav";
+  const response = ffmpeg(newFileName)
+    .toFormat("wav")
+    .on("error", (err) => {
+      console.log(err);
+      res.send("Video has no audio");
+    })
+    .on("end", () => {
+      console.log("Finished");
+      res.redirect("/foundAudio");
+    })
+    .save(targetLocn, () => {});
+  console.log(response);
 });
 
 app.listen(3000, () => {
